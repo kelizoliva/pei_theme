25a92375781228bb358cc6d685dc8c8a5e29dbe9
Jessica Oros <orosjess@gmail.com>
2021-04-02 22:20:30 -0400

    update subquery views

--- template.php
+++ template.php
@@ -4,7 +4,7 @@
  * Override output of some views to be used in math expressions.
  */
 function pei_theme_views_post_render(&$view, &$output, &$cache) {
-  $raw_views = array('count_activities');
+  $raw_views = array('subquery_count_activities');
   if(in_array($view->name, $raw_views)) {
     $base_field = $view->base_field;
     $output = $view->result[0]->$base_field;
