f44002c870828d41bdd8a8ddf8216b56896e30bf
Kristin Elizoliva <kristin@palantetech.coop>
2021-04-07 10:20:10 -0700

    Switched accordion class addition from template to js

--- js/palante_pei.js
+++ js/palante_pei.js
@@ -24,6 +24,7 @@ Backdrop.behaviors.base = {
         return false;
       });
       $('.summary .ui-accordion-content').addClass('container gutters');
+      $('.summary .views-view-accordion').addClass('col-12');
     });
   }
 };
