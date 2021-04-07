3f1b5c07514d513afdc7d684919b8a876cdc9f2d
Jessica Oros <orosjess@gmail.com>
2021-04-07 10:07:35 -0400

    checking for view names beginning with 'subquery_' instead of array

--- template.php
+++ template.php
@@ -4,8 +4,7 @@
  * Override output of some views to be used in math expressions.
  */
 function pei_theme_views_post_render(&$view, &$output, &$cache) {
-  $raw_views = array('subquery_count_activities');
-  if(in_array($view->name, $raw_views)) {
+  if(strpos($view->name, 'subquery_') === 0) {
     $base_field = $view->base_field;
     $output = $view->result[0]->$base_field;
   }
