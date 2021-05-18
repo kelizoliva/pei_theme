1712849cdf619c863441fd02a026fdff1045f8c0
Kristin Elizoliva <kristin@palantetech.coop>
2021-05-17 18:04:08 -0700

    Navigation styling updates

--- js/palante_pei.js
+++ js/palante_pei.js
@@ -40,8 +40,7 @@ $(window).on('load', function (e) {
   $('.view-expanded-views-activities .fieldset-wrapper').not('.view-expanded-views-activities #edit-filters-date .fieldset-wrapper').not('.view-expanded-views-activities #edit-more-filters-retreat-end-date .fieldset-wrapper').addClass('container gutters content-end items-end');
   $('.view-expanded-views-activities #edit-filters-date .fieldset-wrapper, .view-expanded-views-activities #edit-more-filters-retreat-end-date .fieldset-wrapper, .view-expanded-views-activities #edit-more-filters-retreat-end-date .fieldset-wrapper').addClass('container content-end items-end');
   $('.view-expanded-views-activities .fieldset-wrapper #edit-manage-columns-columns').addClass('container gutters content-end items-end');
-  $('.view-expanded-views-activities .sliderfield').addClass('container gutters content-end items-end');
-  
+  $('.view-expanded-views-activities .sliderfield').addClass('container gutters content-end items-end');  
   $('.view-expanded-views-activities .sliderfield-container').addClass('col-12');
   $('.view-expanded-views-activities .sliderfield-event-field-container').addClass('col-12 col-xs-6 col-md-4');
   $('.view-expanded-views-activities .form-item').not('.view-expanded-views-activities fieldset#edit-filters-date .form-item').not('.view-expanded-views-activities fieldset#edit-more-filters-retreat-end-date .form-item').addClass('col-12 col-xs-6 col-md-4');
@@ -49,9 +48,21 @@ $(window).on('load', function (e) {
   $('.view-expanded-views-activities fieldset#edit-more-filters-retreat-end-date').addClass('col-12 col-sm-8');
   $('.view-expanded-views-activities fieldset#edit-filters-date .form-item').addClass('col-12 col-xs-6');
   $('.view-expanded-views-activities fieldset#edit-more-filters-retreat-end-date .form-item').addClass('col-12 col-xs-6');
-  
   // Expanded activities element changes
   $('#pei-activities-form #edit-apply, #pei-activities-form #edit-reset, #pei-activities-form #edit-export').wrapAll('<div class="form-action-buttons"></div>');
+  
+  // Navigation Captions
+  $(function(){
+    var current = location.pathname;
+    $('.navigation a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href') === current ){
+            $this.parents('.navigation').addClass('active');
+            $this.parents('.navigation').find('.nav-caption').addClass('active').prependTo('.l-main');
+        }
+    })
+  })
   
   // Wrapping data in statistics
   var rex = new RegExp("([0-9]+\.?[0-9]+)", "gm");
